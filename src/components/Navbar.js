ef1e058798778dbad9733ab157a16d43300c6f98
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-05-23 00:27:02 +0600

    every folder added

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navItem = (
+    <>
+      <li>
+        <Link to="/">home</Link>
+      </li>
+      <li>
+      <Link to="/about">About</Link>
+      </li>
+      <li>
+      <Link to="/Login">Login</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              ></path>
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItem}
+          </ul>
+        </div>
+        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div class="navbar-end hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">
+        {navItem}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
